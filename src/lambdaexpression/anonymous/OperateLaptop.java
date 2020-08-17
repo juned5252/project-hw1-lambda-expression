01e05d0b83b1cd27de8330b712310e95157a1c2f
Juned alam <Juned5252@gmail.com>
2020-08-16 22:08:09 -0400

    HW1 submission

--- src/lambdaexpression/anonymous/OperateLaptop.java
+++ src/lambdaexpression/anonymous/OperateLaptop.java
@@ -4,16 +4,24 @@ public class OperateLaptop {
 
     public static void main(String[] args) {
         //Please implement anonymous class for interface laptop
-        Laptop laptop = null;
+        Laptop laptop = new Laptop() {
+            @java.lang.Override
+            public void ramSize(int memorySize) {
+                System.out.println("Physical ram on system: " + memorySize + " GB");
+            }
 
+            @java.lang.Override
+            public void monitorSize(int screenSize) {
+                System.out.println("System screen size: " + screenSize+ " in");
+            }
+        };
 
-
-
-        laptop.monitorSize(16);
+        laptop.ramSize(16);
         laptop.monitorSize(15);
 
         //please implement anonymous function for Phone FunctionalInterface
-
+        Phone phone = str -> System.out.println("Sim network: "+ str);
+        phone.cellularNetwork("T-Mobile");
 
     }
 }
